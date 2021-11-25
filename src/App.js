0b617a81095db5d55578db0e6978019642252657
Dan McKinney <adrmckinney@gmail.com>
2021-11-25 03:06:58 -0500

    added main content wrapper and abstracted out the three containers, added snippets list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-import AppHeader from "./components/AppHeader/AppHeader";
+import './App.css'
+import AppHeader from './components/AppHeader/AppHeader'
+import MainContainer from './components/MainContainer/MainContainer'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <AppHeader />
+      <MainContainer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,1 +1,19 @@
-// https://tailwindui.com/components/application-ui/application-shells/multi-column
+import * as React from 'react'
+import ThreeColWrapper from '../_generic/three-col-wrapper'
+import ListCards from './SnippetsList/list-cards'
+
+const MainContainer = () => {
+  return (
+    <>
+      <ThreeColWrapper
+        leftColContent={<ListCards title={'Snippets'} />}
+        centerColContent={<p>This is the center</p>}
+        rightColContent={<p>This is the right</p>}
+        constrainedWidth={false}
+        contrainedHeight={true}
+      />
+    </>
+  )
+}
+
+export default MainContainer
