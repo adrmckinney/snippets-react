4f497e6220a396205d29113b25b17c4ed6276f6a
Dan McKinney <adrmckinney@gmail.com>
2022-01-02 01:39:52 -0500

    another edit

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const apiUrl = axios.create({
-  // baseURL: process.env.REACT_APP_LOCAL_API_URL || 'https://mckinney-snippets.herokuapp.com/',
-  baseURL: 'https://mckinney-snippets.herokuapp.com/',
+  // baseURL: process.env.REACT_APP_LOCAL_API_URL || 'https://mckinney-snippets.herokuapp.com/api/',
+  baseURL: 'https://mckinney-snippets.herokuapp.com/api/',
 })
